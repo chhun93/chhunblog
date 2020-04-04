2e7596d8043cb1978f8916949c0cc87d47ee23b0
chhun93 <chhun3830@gmail.com>
2020-04-04 16:49:21 +0900

    posts 꾸미기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import HeaderLg from "./Header/LargeHeader";
 import Posts from "./Contents/Posts";
+import './DecorateFiles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
--- src/Contents/Posts.js
+++ src/Contents/Posts.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+import "../DecorateFiles/Posts.css";
 
 class Posts extends Component {
+  state = {
+    posts: []
+  };
   render = () => {
-    return (<div>Hello_Posts</div>)
+    return (
+      <div>
+        <Container className="Post_Container">
+          <Row>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+          </Row>
+          <Row>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+          </Row>
+
+          <Row>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+            <Col>Hello_Posts</Col>
+          </Row>
+          <Row>
+            <Col>
+              <Spinner
+                size="sm"
+                animation="border"
+                role="status"
+                variant="secondary"
+              />
+            </Col>
+            <Col>
+              <Spinner
+                size="sm"
+                animation="border"
+                role="status"
+                variant="secondary"
+              />
+            </Col>
+            <Col>
+              <Spinner
+                size="sm"
+                animation="border"
+                role="status"
+                variant="secondary"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   };
 }
 
