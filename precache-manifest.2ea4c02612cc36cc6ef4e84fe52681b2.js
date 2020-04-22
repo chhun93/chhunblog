6da991be1f56144eb5e2b153198b621dc1537b9f
chhun93 <chhun3830@gmail.com>
2020-04-22 20:54:44 +0900

    Updates

---
+++ precache-manifest.2ea4c02612cc36cc6ef4e84fe52681b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242e08fda484e5a287a6c2e9ce370296",
+    "url": "/chhunblog/index.html"
+  },
+  {
+    "revision": "3ab7fd57ffd205b61b16",
+    "url": "/chhunblog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "548bad409f6b67a844dd",
+    "url": "/chhunblog/static/css/main.8351165f.chunk.css"
+  },
+  {
+    "revision": "3ab7fd57ffd205b61b16",
+    "url": "/chhunblog/static/js/2.cdf5acc6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/chhunblog/static/js/2.cdf5acc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548bad409f6b67a844dd",
+    "url": "/chhunblog/static/js/main.81301224.chunk.js"
+  },
+  {
+    "revision": "09c0837adcdd55e84be4",
+    "url": "/chhunblog/static/js/runtime-main.b1a9f4b7.js"
+  }
+]);
