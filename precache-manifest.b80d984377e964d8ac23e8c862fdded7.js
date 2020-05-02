72ebd5238da7ce6dcec745f2252c59c95019ca17
chhun93 <chhun3830@gmail.com>
2020-05-02 22:53:18 +0900

    Updates

---
+++ precache-manifest.b80d984377e964d8ac23e8c862fdded7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f822439540de11bdc7f8219564d9851",
+    "url": "/chhunblog/index.html"
+  },
+  {
+    "revision": "fc54c6552dc58a79fcd1",
+    "url": "/chhunblog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "471b4fc7740bb4129f62",
+    "url": "/chhunblog/static/css/main.8b8024e3.chunk.css"
+  },
+  {
+    "revision": "fc54c6552dc58a79fcd1",
+    "url": "/chhunblog/static/js/2.bbe43d25.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/chhunblog/static/js/2.bbe43d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471b4fc7740bb4129f62",
+    "url": "/chhunblog/static/js/main.8564daf4.chunk.js"
+  },
+  {
+    "revision": "09c0837adcdd55e84be4",
+    "url": "/chhunblog/static/js/runtime-main.b1a9f4b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9218fd6e433a10900d8a0cbc7133a8a4.js"
+  "/chhunblog/precache-manifest.b80d984377e964d8ac23e8c862fdded7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chhunblog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
